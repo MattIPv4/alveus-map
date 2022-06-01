56aee4213248b19715dc1d650205ada0bfe62bac
MattIPv4 <me@mattcowley.co.uk>
2022-06-01 01:30:19 +0100

    Allow zooming out beyond scale 1

--- src/map.js
+++ src/map.js
@@ -1,60 +1,79 @@
 import svgPanZoom from 'svg-pan-zoom';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const map = document.getElementsByTagName('svg')[0];
-    const maxZoomMult = 15;
+const clampPosition = (pz, pos) => {
+    // Ensure all the edges remain pinned to the viewport edges
+    // Or, ensure we're pinned to the center if smaller than the viewport
+    const sizes = pz.getSizes();
+    const width = sizes.viewBox.width * sizes.realZoom;
+    const height = sizes.viewBox.height * sizes.realZoom;
+    const extraWidth = Math.max(sizes.width - width, 0) / 2;
+    const extraHeight = Math.max(sizes.height - height, 0) / 2;
 
-    const clampPosition = (pz, pos) => {
-        // Ensure all the edges remain pinned to the viewport edges
-        const sizes = pz.getSizes();
-        const maxWidth = (sizes.viewBox.width * sizes.realZoom) - sizes.width;
-        const maxHeight = (sizes.viewBox.height * sizes.realZoom) - sizes.height;
-        return {
-            x: Math.max(Math.min(pos.x, 0), -maxWidth),
-            y: Math.max(Math.min(pos.y, 0), -maxHeight),
-        };
+    // min(x, val) = how far pulled to the right from the left edge
+    // max(x, val) = how far pulled to the left from the right edge, offset by the width minus the viewport width
+    // min(y, val) = how far pulled to the bottom from the top edge
+    // max(y, val) = how far pulled to the top from the bottom edge, offset by the height minus the viewport height
+    return {
+        x: Math.max(Math.min(pos.x, extraWidth), -(extraWidth + width - sizes.width)),
+        y: Math.max(Math.min(pos.y, extraHeight), -(extraHeight + height - sizes.height)),
     };
+};
 
-    const computeMinZoom = pz => {
-        // Trigger panZoom to update the viewBox
-        pz.resize();
+const computeMinZoom = (pz, minZoom, maxZoom, applyBase = false) => {
+    // Trigger panZoom to update the viewBox
+    pz.resize();
 
-        // Ensure the minimum zoom fills the screen
-        const currentZoom = pz.getZoom();
-        const currentSizes = pz.getSizes();
-        const actualWidth = currentSizes.viewBox.width * currentSizes.realZoom;
-        const widthRatio = currentSizes.width / actualWidth;
-        const actualHeight = currentSizes.viewBox.height * currentSizes.realZoom;
-        const heightRatio = currentSizes.height / actualHeight;
+    // Ensure the minimum zoom fills the screen
+    const currentSizes = pz.getSizes();
+    const actualWidth = currentSizes.viewBox.width * currentSizes.realZoom;
+    const widthRatio = currentSizes.width / actualWidth;
+    const actualHeight = currentSizes.viewBox.height * currentSizes.realZoom;
+    const heightRatio = currentSizes.height / actualHeight;
+    const baseZoom = Math.max(widthRatio, heightRatio) * pz.getZoom();
 
-        // Apply the new minimum zoom
-        const minZoom = Math.max(widthRatio, heightRatio) * currentZoom;
-        pz.setMinZoom(minZoom);
-        if (pz.getZoom() < minZoom) {
-            pz.zoom(minZoom);
-            pz.pan(clampPosition(pz, pz.getPan()));
-        }
+    // Apply the base zoom if requested
+    if (applyBase) {
+        pz.zoom(baseZoom);
+        pz.center();
+        pz.pan(clampPosition(pz, pz.getPan()));
+    }
 
-        // Apply the new maximum zoom
-        const maxZoom = minZoom * maxZoomMult;
-        pz.setMaxZoom(maxZoom);
-        if (pz.getZoom() > maxZoom) {
-            pz.zoom(maxZoom);
-            pz.pan(clampPosition(pz, pz.getPan()));
-        }
-    };
+    // Apply the new minimum zoom
+    const newMinZoom = baseZoom * minZoom;
+    pz.setMinZoom(newMinZoom);
+    if (pz.getZoom() < newMinZoom) {
+        pz.zoom(newMinZoom);
+        pz.pan(clampPosition(pz, pz.getPan()));
+    }
 
-    const panZoom = svgPanZoom(map, {
-        center: true,
+    // Apply the new maximum zoom
+    const newMaxZoom = baseZoom * maxZoom;
+    pz.setMaxZoom(newMaxZoom);
+    if (pz.getZoom() > newMaxZoom) {
+        pz.zoom(newMaxZoom);
+        pz.pan(clampPosition(pz, pz.getPan()));
+    }
+};
+
+const startPanZoom = svg => {
+    const minZoom = 0.5;
+    const maxZoom = 5;
+
+    const panZoom = svgPanZoom(svg, {
         zoomScaleSensitivity: 0.5,
-        minZoom: 1,
-        maxZoom: maxZoomMult,
+        minZoom,
+        maxZoom,
         beforePan(_, newPan) { return clampPosition(this, newPan); },
     });
 
-    computeMinZoom(panZoom);
+    computeMinZoom(panZoom, minZoom, maxZoom, true);
     window.addEventListener('resize', () => {
-        computeMinZoom(panZoom);
+        computeMinZoom(panZoom, minZoom, maxZoom);
         panZoom.pan(clampPosition(panZoom, panZoom.getPan()));
     });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const map = document.getElementsByTagName('svg')[0];
+    startPanZoom(map);
 });
