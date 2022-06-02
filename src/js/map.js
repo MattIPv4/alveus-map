ce094c2dbb6c7c82c041fde97fb877b376d60a34
MattIPv4 <me@mattcowley.co.uk>
2022-06-02 23:23:59 +0100

    Add some animated birds to the map

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,11 @@
 import infoOutlines from './infoOutlines';
 import panZoom from './panZoom';
+import animateMap from './animateMap';
 
 document.addEventListener('DOMContentLoaded', () => {
     const map = document.getElementsByTagName('svg')[0];
     const info = document.getElementById('info');
     infoOutlines(map, info);
+    animateMap(map);
     panZoom(map);
 });
